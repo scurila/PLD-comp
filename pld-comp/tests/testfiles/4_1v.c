77c932b1fdcf53252a5603efbe258ff6c47f2a36
Arthur Gardon <arthur.gardon@gmail.com>
2022-03-09 08:59:25 +0100

    testing hell

---
+++ pld-comp/tests/testfiles/4_1v.c
@@ -0,0 +1,8 @@
+int main(){
+    int x, y, z;
+    x = 4;
+    y = 5;
+    z = 3;
+    x = z - y;
+    return x;
+}
---
+++ pld-comp/tests/testfiles/4_5v.c
@@ -0,0 +1,9 @@
+int main(){
+    int a, b, c, x, y, z;
+    x = 4;
+    y = 5;
+    z = 3;
+
+    x = x * 20 - y * 10 + z * x;
+    return x;
+}

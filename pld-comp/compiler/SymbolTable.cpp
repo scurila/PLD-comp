fbcf299664c44ccc12213812033ed64719fba34a
dblasko <daniel.blasko.dev@gmail.com>
2022-03-13 23:52:50 +0100

    Handle var offset increment based on size

--- pld-comp/compiler/SymbolTable.cpp
+++ pld-comp/compiler/SymbolTable.cpp
@@ -1,14 +1,15 @@
 #include "SymbolTable.h"
 #include "Exceptions.h"
 
+using namespace std;
+
 bool SymbolTable::addEntry(string name, string type) {
 	int size = 4;
-	// todo : tailles selon type
-	/*for (int i = 0; i < sizeof(VarTypeName)/sizeof(VarTypeName[0]) ; i++) { 
+	for (int i = 0; i < sizeof(VarTypeName)/sizeof(VarTypeName[0]) ; i++) { 
 		if (type == VarTypeName[i]) {
 			size = VarTypeSize[i];
 		}
-	}*/
+	}
     if (table.find(name) == table.end()) {
 		// on crée la var°
 		table.insert(make_pair(name, new Entry(name, type, topOffset + size, size)));

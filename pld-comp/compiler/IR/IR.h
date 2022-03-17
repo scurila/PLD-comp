442941660a12bd6e440c159967a56d117a1aa1bf
dblasko <daniel.blasko.dev@gmail.com>
2022-03-17 09:26:52 +0100

    Adapt IRInstr

--- pld-comp/compiler/IR/IR.h
+++ pld-comp/compiler/IR/IR.h
@@ -38,19 +38,17 @@ public:
 	} Operation;
 
 	/**  constructor */
-	IRInstr(BasicBlock *bb_, Operation op, Type t, vector<string> params);
+	IRInstr(BasicBlock *bb_, Operation op, Type t);
 
 	/** Actual code generation */
 	void gen_asm(ostream &o, bool x86); /** ARM generation wrapper (calls x86 or arm generator based on flag) */
-	void gen_x86(ostream &o); /** < x86 assembly code generation for this IR instruction */
-	void gen_arm(ostream &o); /** < M1 ARM assembly code generation for this IR instruction */
+	virtual void gen_x86(ostream &o) = 0; /** < x86 assembly code generation for this IR instruction */
+	virtual void gen_arm(ostream &o) = 0; /** < M1 ARM assembly code generation for this IR instruction */
 
 private:
 	BasicBlock *bb; /**< The BB this instruction belongs to, which provides a pointer to the CFG this instruction belong to */
 	Operation op;
 	Type t;
-	vector<string> params; /**< For 3-op instrs: d, x, y; for ldconst: d, c;  For call: label, d, params;  for wmem and rmem: choose yourself */
-						   // if you subclass IRInstr, each IRInstr subclass has its parameters and the previous (very important) comment becomes useless: it would be a better design.
 };
 
 /**  The class for a basic block */

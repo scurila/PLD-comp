135eccb475fd8f186d528249d4988d424eeade78
dblasko <daniel.blasko.dev@gmail.com>
2022-03-17 19:24:44 +0100

    IR Add COPY

---
+++ pld-comp/compiler/IR/IRInstr_copy.h
@@ -0,0 +1,14 @@
+#include "IR.h"
+#include <string>
+
+class IRInstr_copy : IRInstr {
+    public:
+        IRInstr_copy(BasicBlock *bb_, string d, string v) : IRInstr(bb_, copy, ""), dest(d), val(v) {};
+        
+        virtual void gen_x86(ostream &o); /** < x86 assembly code generation for this IR instruction */
+        virtual void gen_arm(ostream &o); /** < M1 ARM assembly code generation for this IR instruction */
+
+        // copy dest val ↔️ dest = val; 
+        string dest;
+        string val;
+};

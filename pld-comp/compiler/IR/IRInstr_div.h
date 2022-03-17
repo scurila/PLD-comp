f9dd4d168704bd09d082bdd19e31986a73acb63c
dblasko <daniel.blasko.dev@gmail.com>
2022-03-17 10:35:22 +0100

    IR instr corrections

--- pld-comp/compiler/IR/IRInstr_div.h
+++ pld-comp/compiler/IR/IRInstr_div.h
@@ -4,9 +4,8 @@
 
 class IRInstr_div.h : IRInstr {
     public:
-            IRInstr_div(BasicBlock *bb_, Operation op, Type t): IRInstr(BasicBlock *bb_, Operation op, Type t){};
+            IRInstr_div(BasicBlock *bb_, Operation op, string t): IRInstr(BasicBlock *bb_, Operation op, string t){};
 
             virtual void gen_x86(ostream &o); /** < x86 assembly code generation for this IR instruction */
             virtual void gen_arm(ostream &o); /** < M1 ARM assembly code generation for this IR instruction */
-    private:
 }
--- pld-comp/compiler/IR/IRInstr_mul.h
+++ pld-comp/compiler/IR/IRInstr_mul.h
@@ -2,10 +2,10 @@
 
 #include "IR.h"
 
-class IRInstr_mul.h : IRInstr {
+class IRInstr_mul : IRInstr {
     public:
-            IRInstr_mul(BasicBlock *bb_, Operation op, Type t): IRInstr(BasicBlock *bb_, Operation op, Type t){};
+            IRInstr_mul(BasicBlock *bb_, Operation op, string t): IRInstr(BasicBlock *bb_, Operation op, string t){};
 
             virtual void gen_x86(ostream &o); /** < x86 assembly code generation for this IR instruction */
             virtual void gen_arm(ostream &o); /** < M1 ARM assembly code generation for this IR instruction */
-    private:
+}

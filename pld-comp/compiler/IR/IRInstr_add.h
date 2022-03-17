e85c306c3934983d3eef4dd6c70967a2014c30ac
Indarren <61293257+Indarren@users.noreply.github.com>
2022-03-17 09:59:43 +0100

    instradd

--- pld-comp/compiler/IR/IRInstr_add.h
+++ pld-comp/compiler/IR/IRInstr_add.h
@@ -1,10 +1,14 @@
+#pragma once
+
 #include "IR.h"
 
 class IRInstr_add : IRInstr {
     public:
         IRInstr_add(BasicBlock *bb_, Operation op, Type t, vector<string> params) : IRInstr(BasicBlock *bb_, Operation op, Type t, vector<string> params);
         
+        virtual void gen_x86(ostream &o); /** < x86 assembly code generation for this IR instruction */
+        virtual void gen_arm(ostream &o); /** < M1 ARM assembly code generation for this IR instruction */
 
-    protected:
+    private:
 
 }

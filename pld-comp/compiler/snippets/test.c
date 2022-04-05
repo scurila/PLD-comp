b234801fb30e975404e00babffc113a714569c34
rgalle <romain.galle@insa-lyon.fr>
2022-04-05 18:41:14 +0200

    Added POC for separate-file compilation

---
+++ pld-comp/compiler/snippets/test.c
@@ -0,0 +1,44 @@
+#include "stdio.h"
+
+void test() {
+    int a = 5;
+
+    return;
+}
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int whois(int a) {
+    return a;
+}
+
+int returnfour() {
+    return 4;
+}
+
+int main()
+{
+    int a = 5;
+    int b = 2;
+    int c;
+
+    int z = 3;
+    int y;
+    
+    {
+        int z;
+        a = 4;
+        z = 2;
+        int y = 2;
+
+        c = a - b + z;
+    }
+
+    z = returnfour();
+
+    z = z + hello();
+
+    return z;  // should be 102+2=104
+}
---
+++ pld-comp/compiler/snippets/test_f2.c
@@ -0,0 +1,13 @@
+
+int hello(int a) {
+    putchar('h');
+    putchar('e');
+    putchar('l');
+    putchar('l');
+    putchar('o');
+    putchar('!');
+    putchar(13);
+    putchar(10);
+
+    return a * 5;
+}
